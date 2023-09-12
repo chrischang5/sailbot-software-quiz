293857c423d16184bb82f506afa1c78967ffb91c
Chris Chang <1chang1chang1@gmail.com>
2023-09-11 20:07:01 -0700

    First attempt

--- python/standard_calc.py
+++ python/standard_calc.py
@@ -11,7 +11,30 @@ def bound_to_180(angle):
     Returns:
         float: The bounded angle in degrees.
     """
-    return 0
+
+    LOWER_BOUND = -180
+    UPPER_BOUND = 180
+    FULL_ANGLE = 360
+
+    if LOWER_BOUND <= angle and angle < UPPER_BOUND:
+        return angle
+    
+    print(angle)
+    # remove any "winding" first: where the angle spans the circle multiple times.
+    angle_without_winding = angle % FULL_ANGLE
+    print(angle_without_winding)
+
+    # Cases: Angle is over upper bound or angle is under lower bound 
+
+    if angle_without_winding > UPPER_BOUND:
+        # We wandered into the 0 to -180 region
+        return LOWER_BOUND + (angle_without_winding % UPPER_BOUND)
+    elif angle_without_winding < LOWER_BOUND:
+        # we wandered into the 0 to 180 region
+        return UPPER_BOUND - (angle_without_winding % LOWER_BOUND)
+    else:
+        # otherwise 
+        return angle_without_winding
 
 
 def is_angle_between(first_angle, middle_angle, second_angle):
@@ -29,4 +52,4 @@ def is_angle_between(first_angle, middle_angle, second_angle):
     Returns:
         bool: True when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, false otherwise.
     """
-    return True
+    return first_angle <= middle_angle and middle_angle <= second_angle 
